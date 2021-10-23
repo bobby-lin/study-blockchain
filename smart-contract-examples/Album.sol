a756c9b74fc5541135f8ef1b679f6c89a6e80909
Bobby Lin <mr.linjiahao@gmail.com>
2021-10-23 12:09:47 +0800

    Update Album.sol

--- smart-contract-examples/Album.sol
+++ smart-contract-examples/Album.sol
@@ -2,7 +2,29 @@
 pragma solidity ^0.7.0;
 
 contract Album {
-    string artist = 'Linkin Park';
-    string albumTitle = 'Hybrid Theory';
-    uint tracks = 12;
+    // Local state variables
+    string public artist;
+    string public albumTitle;
+    uint public tracks;
+    // Constant cannot be reassigned
+    string public constant contractAuthor = "Bobby Lin";
+    
+    constructor() {
+        artist = 'Linkin Park';
+        albumTitle = 'Hybrid Theory';
+        tracks = 12;
+    }
+    
+    // For returns, we specify what will be returned using returns (...)
+    function getAlbum() public returns (string memory, string memory, uint) {
+        return (artist, albumTitle, tracks);
+    }
+    
+    // For set, we specify the params that will be used to modify the local variables of the SC.
+    function setAlbum(string memory _artist, string memory _albumTitle, uint _tracks) public {
+        artist = _artist;
+        albumTitle = _albumTitle;
+        tracks = _tracks;
+    }
+    
 }
