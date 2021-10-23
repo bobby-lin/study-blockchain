03658146e9916805e0f686407106659cd08cec1f
Bobby Lin <mr.linjiahao@gmail.com>
2021-10-23 11:38:11 +0800

    Create Album.sol

---
+++ smart-contract-examples/Album.sol
@@ -0,0 +1,8 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.7.0;
+
+contract Album {
+    string artist = 'Linkin Park';
+    string albumTitle = 'Hybrid Theory';
+    uint tracks = 12;
+}
