178e78d917853c7c04d744f4b5bd2c3fd85c75bb
Bobby Lin <mr.linjiahao@gmail.com>
2022-07-13 09:14:32 +0800

    Create StorageFactory.sol

---
+++ smart-contract-examples/freecodecamp/StorageFactory.sol
@@ -0,0 +1,23 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.8;
+
+import './SimpleStorage.sol';
+
+contract StorageFactory {
+    SimpleStorage[] public simpleStorageArray;
+
+    function createSimpleStorageContract() public {
+        SimpleStorage simpleStorage = new SimpleStorage();
+        simpleStorageArray.push(simpleStorage);
+    }
+
+    function sfStore(uint256 _simpleStorageIndex, uint256 _simpleStorageNumber) public {
+        simpleStorageArray[_simpleStorageIndex].store(_simpleStorageNumber);
+    }
+
+    function sfGet(uint256 _simpleStorageIndex) public view returns(uint256) {
+        return simpleStorageArray[_simpleStorageIndex].retrieve();
+    }
+
+}
